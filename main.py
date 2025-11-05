4854d67e911a265359b367e5383839095d2de64c
Rashicom <rashid.kp484@gmail.com>
2025-11-05 17:09:34 +0530

    streamlit configured

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-from dotenv import dotenv_values
-from langchain.chat_models import init_chat_model
-from langchain.agents import create_agent
-from langgraph.checkpoint.memory import InMemorySaver
-from dataclasses import dataclass
+import streamlit as st
+import time
 
+st.header("KnowChat-V2")
 
-config = dotenv_values(".env")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# display message
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+    
 
-model = init_chat_model("google_genai:gemini-2.5-flash-lite", google_api_key=config.get("GEMINI_API_KEY"))
-checkpointer = InMemorySaver()
+if prompt := st.chat_input("what's up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state.messages.append({"role":"user", "content":prompt})
 
-@dataclass
-class Context:
-    """Custom runtime context schema."""
-    user_id: str
+    # pass the prompt to llm as stream
 
-# print(model.invoke("hello there"))
-def get_weather(city: str) -> str:
-    """Get weather for a given city."""
-    return {"temperature":"30 degree celsius", "wind":"15 km/hour"}
-
-agent = create_agent(
-    model=model,
-    system_prompt="You are a helpful assistant",
-    tools=[get_weather],
-    # checkpointer=checkpointer
-)
-
-# config = {"configurable": {"thread_id": "1"}}
-
-for chunk in agent.stream(
-    {"messages":[{"role":"user", "content":"what is the wether in kochi"}]},
-    stream_mode="updates"
-):
-    for step, data in chunk.items():
-        print("step : ", step)
-        print("content", data['messages'][-1].content_blocks)
-        print("\n")
-
-# res = agent.invoke(
-#     {"messages": [{"role": "user", "content": "my name is rashid"}]},
-#     config = {"configurable": {"thread_id": "1"}},
-#     context=Context(user_id="1")
-# )
-
+    with st.chat_message("bot"):
+        status_placeholder = st.empty()
+        for stage in ["thinging","searching","content response"]:
+            status_placeholder.markdown(stage)
+            time.sleep(1)
+        
+        st.session_state.messages.append({"role":"bot", "content":stage})
